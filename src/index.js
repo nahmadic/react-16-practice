5212d39a38c0fa6e2c030e3967dc271febfd6e4b
Ryan Browne <brownetown7@gmail.com>
2019-08-06 22:00:32 +0100

    Adds logo file
    Adds logo file and adds to app component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import logo from "./logo.svg";
+
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
     </div>
   );
 }
